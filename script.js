3945023377828447f3355226429026b96d1d2ebd
MaximeBourgeois <maxime.bourgeois78@gmail.com>
2021-11-16 09:50:06 +0100

    ajouter des item dans la liste

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ for (let i = 0; i < stopPropagationForm.length; i++) {
     })
 }
 
+//créer un nouvel élément dans la liste en cliquant sur le bouton "Ajouter"
+function newElement() {
+  let li = document.createElement("li");
+  console.log(li)
+  let inputValue = document.getElementById("inputItem").value;
+  //créer un noeud de texte, enfant de la liste
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("listItem").appendChild(li);
+  }
+  document.getElementById("inputItem").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
